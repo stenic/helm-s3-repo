877139c6695c6e8dd915b9448c95fdaf2db6f17d
Dries De Peuter <dries@nousefreak.be>
2020-04-28 12:14:13 +0200

    Allow to set arguments

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,6 +3,8 @@
 set -e
 
 S3_BUCKET_ACL=${S3_BUCKET_ACL:-public-read}
+PACKAGE_ARGS=${PACKAGE_ARGS}
+PUSH_ARGS=${PUSH_ARGS:---ignore-if-exists}
 
 echo "==> Init"
 [[ ! -z "${S3_BUCKET}" ]] || (echo "ERROR: S3_BUCKET variable is required" && exit 1)
@@ -18,9 +20,9 @@ while IFS= read -r d; do
     echo "==> Found ${d}"
 
     echo -n " - packaging: "
-    helm package ${d}
+    helm package ${PACKAGE_ARGS} ${d}
 
     echo -n " - pushing:   "
-    helm s3 push --acl=${S3_BUCKET_ACL} --ignore-if-exists ${d}-*.tgz myrepo
+    helm s3 push --acl=${S3_BUCKET_ACL} ${PUSH_ARGS} ${d}-*.tgz myrepo
     echo "Pushed to ${S3_BUCKET}"
 done < <(find * -prune -type d)
